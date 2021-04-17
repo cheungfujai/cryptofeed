246c22008ff7f02cdec046e3de22199796eabb28
richardlyk1108 <richardlyk1108@gmail.com>
2021-04-18 01:19:42 +0800

    asset controller api

--- src/main/java/com/myproject/cryptoapi/lunarcrushapi/interfaces/AssetController.java
+++ src/main/java/com/myproject/cryptoapi/lunarcrushapi/interfaces/AssetController.java
@@ -1,5 +1,6 @@
 package com.myproject.cryptoapi.lunarcrushapi.interfaces;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myproject.cryptoapi.lunarcrushapi.domain.asset.Asset;
 import com.myproject.cryptoapi.lunarcrushapi.domain.asset.AssetService;
 import com.myproject.cryptoapi.lunarcrushapi.infrastructure.connector.lunarcrush.LunarcrushService;
@@ -7,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class AssetController {
@@ -20,14 +25,15 @@ public class AssetController {
     }
 
     @GetMapping(value = URL + "/asset/data")
-    public Asset getTodayCryptoData(@RequestParam String data,
-                                    @RequestParam String apiKey,
-                                    @RequestParam String symbol){
-        return lunarcrushService.getAsset(data, apiKey, symbol);
+    public Asset getTodayCryptoData() throws URISyntaxException {
+        URI uri = new URI("https://api.lunarcrush.com/v2?data=assets&key=arayehysw9a0g01t1pj9dkd&symbol=LTC");
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.getForObject(uri,Asset.class);
     }
 
     @GetMapping(value = URL + "/test")
-    public String getTodayCryptoData(){
+    public String getTodayData(){
         return "hi";
     }
 }
